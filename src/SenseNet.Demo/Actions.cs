b25d7d9d4da7e7f401d2ecf02d95a673e317c5b8
tusmester <tusmester@gmail.com>
2017-08-10 16:06:58 +0200

    Initial commit.

---
+++ src/SenseNet.Demo/Actions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using SenseNet.ApplicationModel;
+using SenseNet.ContentRepository;
+using SenseNet.ContentRepository.Storage.Security;
+
+namespace SenseNet.Demo
+{
+    public static class Actions
+    {
+        [ODataAction]
+        public static Content RegisterUser(Content content, string email, string password)
+        {
+            if (string.IsNullOrEmpty(email))
+                throw new ArgumentNullException(nameof(email));
+            if (string.IsNullOrEmpty(password))
+                throw new ArgumentNullException(nameof(password));
+
+            using (new SystemAccount())
+            {
+                //TODO: determine custom user content type
+                var user = Content.CreateNew("User", content.ContentHandler, email);
+                user["Email"] = email;
+                user.Save();
+
+                return user;
+            }
+        }
+    }
+}
---
+++ tools/scripts/CreateSnAdminPackages.ps1
@@ -0,0 +1,12 @@
+﻿$srcPath = [System.IO.Path]::GetFullPath(($PSScriptRoot + '\..\..\src'))
+$packageSourcePath = "$srcPath\snadmin\install-demo"
+$packagePath = "$srcPath\snadmin\install-demo.zip"
+
+if (!(Test-Path "$packageSourcePath\bin"))
+{
+	New-Item "$packageSourcePath\bin" -Force -ItemType Directory
+}
+
+Copy-Item $srcPath\SenseNet.Demo\bin\Release\SenseNet.Demo.* "$packageSourcePath\bin" -Force
+
+Compress-Archive -Path "$packageSourcePath\*" -Force -CompressionLevel Optimal -DestinationPath $packagePath
